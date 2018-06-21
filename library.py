1067dc1a859c6ad0c9b48fc8aef65e577e1db2bb
Rachana Gupta <rachanagupta@Rachanas-MacBook-Pro.local>
2018-06-21 15:46:50 +0530

    TDD tests

--- library.py
+++ library.py
@@ -5,19 +5,47 @@ _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+_date_fm2_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
+
+_test_date_fm2_pat = re.compile(r'\d{2} (Jan|Feb|Mar|Apr|Jul|Aug|Sep|Oct|Nov|Dec)(,?) \d{4}')
+_test_date_iso8601_pat = re.compile(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])( |T)\d{2}:\d{2}:\d{2}.\d{3}(([A-Z]{3}|[A-Z])+|-\d{3})*')
+_test_integer_pat = re.compile(r'(\d{1,3},?)+($| )')
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
     for match in _mixed_ordinal_pat.finditer(text):
         yield('ordinal', match)
 
+def dates_iso8601(text):
+    '''Find date in pattern like 2015-13-48'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('ordinal', match)
+
+def test_dates_iso8601(text):
+    '''Find date in pattern like 2015-13-48'''
+    for match in _test_date_iso8601_pat.finditer(text):
+        yield('ordinal', match)
+
+def dates_fmt2(text):
+    '''Find date in pattern like 25 Jan 2018'''
+    for match in _date_fm2_pat.finditer(text):
+        yield('ordinal', match)
+
 def integers(text):
     '''Find integers in text. Don't count floating point numbers.'''
     for match in _integer_pat.finditer(text):
-        # If the integer we're looking at is part of a floating-point number, skip it.
+       # If the integer we're looking at is part of a floating-point number, skip it.
         if _floating_point_before_pat.match(text, match.start()) or \
                 _floating_point_after_pat.match(text, match.end()):
             continue
+        yield ('integer', match)
+
+def test_integers(text):
+    '''Find integers in text. Don't count floating point numbers.'''
+    for match in _test_integer_pat.finditer(text):
+        # If the integer we're looking at is part of a floating-point number, skip it.
+       
         yield ('integer', match)
 
 def scan(text, *extractors):
@@ -29,3 +57,8 @@ def scan(text, *extractors):
     for extractor in extractors:
         for item in extractor(text):
             yield item
+
+def test_dates_fmt2(text):
+    '''Find date in pattern like 25 Jan, 2018'''
+    for match in _test_date_fm2_pat.finditer(text):
+        yield('ordinal', match)
--- library_test.py
+++ library_test.py
@@ -29,6 +29,62 @@ class TestCase(unittest.TestCase):
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
+    
+    # Fourth unit test; test date with format like 2015-07-25
+    def test_date(self):
+        self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, '2015-07-25')
+
+    # Fifth unit test; test date with invalid months/days
+    def test_incorrectDate(self):
+        self.assert_extract("2015-13-48", library.test_dates_iso8601)
+
+    # test date with format 25 Jan 2017
+    def test_dates_fmt2(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.test_dates_fmt2, '25 Jan 2017')
+   
+   # Assignment tests - 
+
+
+
+    # Test 1 - test iso8601 date with timeStamp with format 2018-06-22 18:22:19.123
+    def test_timeStamp1(self):
+        self.assert_extract('Current timeStamp is 2018-06-22 18:22:19:123', library.test_dates_iso8601, '2018-06-22 18:22:19:123')
+
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123
+    def test_timeStamp2(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123', library.test_dates_iso8601, '2018-06-22T18:22:19.123')
+    
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123MDT
+    def test_timeStamp3(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123MDT', library.test_dates_iso8601, '2018-06-22T18:22:19.123MDT')
+
+    # test  iso8601 timeStamp with format 2018-06-22T18:22:19.123Z
+    def test_timeStamp4(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123Z', library.test_dates_iso8601, '2018-06-22T18:22:19.123Z')
+
+     # test iso8601 timeStamp with format 2018-06-22T18:22:19.123-800
+    def test_timeStamp5(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123-800', library.test_dates_iso8601, '2018-06-22T18:22:19.123-800')
+
+     # test iso8601 timeStamp with wrong format 
+    def test_timeStamp6(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.12', library.test_dates_iso8601)    
+
+    # test date format 2 with comma like 25 Jun, 2017 
+    def test_date_fmt2_comma(self):
+        self.assert_extract('Current date is 25 Jul, 2017', library.test_dates_fmt2, '25 Jul, 2017')
+
+       # test date format 2 with comma like 25 Jun, 2017
+    def test_date_fmt2_comma(self):
+        self.assert_extract('Current date is 25 July, 2017', library.test_dates_fmt2)
+        
+    # test number with comma separated groupings
+    def test_integers_comma(self):
+        self.assert_extract('I have 145,345', library.test_integers, '145,345')
+
+         # test number with comma separated groupings
+    def test_invalid_integers_comma(self):
+        self.assert_extract('I have 145,345d', library.test_integers)
 
 
 if __name__ == '__main__':
